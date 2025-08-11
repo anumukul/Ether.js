c86e5c04dcd7a9112a8a55607c3bed2f9bb21b69
Anubhav <anumukul009@gmail.com>
2025-08-12 01:35:47 +0530

    writing to smart contracts

--- examples/4_write_smart_contracts.cjs
+++ examples/4_write_smart_contracts.cjs
@@ -1,49 +1,65 @@
 require("dotenv").config()
 const { ethers } = require("ethers")
 
-// Import private key helper
+
 const { promptForKey } = require("../helpers/prompt.js")
 
-// Setup connection
-const URL = process.env.TENDERLY_RPC_URL
+
+const URL = process.env.tenderly_rpc_url;
 const provider = new ethers.JsonRpcProvider(URL)
 
-// Define "Application Binary Interface"
+
 const ERC20_ABI = [
   "function decimals() view returns (uint8)",
   "function balanceOf(address) view returns (uint256)",
   "function transfer(address to, uint amount) returns (bool)",
 ];
 
-// Setup contract
+
 const ERC20_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" // USDC Contract
 const contract = new ethers.Contract(ERC20_ADDRESS, ERC20_ABI, provider)
 
-// Define reciever
-const RECIEVER = "" // Your account address 2
+
+const RECIEVER = "0x6AFdcc2ff48cbEdE371F35C2083E62E1fF812b90" ;
+
 
 async function main() {
   const privateKey = await promptForKey()
 
-  // Setup wallet
+  
   const wallet = new ethers.Wallet(privateKey, provider)
 
-  // Get ERC20 balances
+  
   const senderBalanceBefore = await contract.balanceOf(wallet.address)
   const recieverBalanceBefore = await contract.balanceOf(RECIEVER)
 
-  // Log ERC20 balances
+ 
   console.log(`\nReading from ${ERC20_ADDRESS}\n`)
   console.log(`Sender balance before: ${senderBalanceBefore}\n`)
   console.log(`Reciever balance before: ${recieverBalanceBefore}\n`)
 
   // Setup amount to transfer
 
+  const amount=ethers.parseUnits("1",6);
+    
+
+
   // Create transaction
+
+  const transaction=await contract.connect(wallet).transfer(RECIEVER,amount);
+ 
+
+
+  
 
   // Wait transaction
 
+     transaction.wait();
+
   // Log transaction
+
+  console.log(transaction);
+
 
   // Get ERC20 balances
   const senderBalanceAfter = await contract.balanceOf(wallet.address)
